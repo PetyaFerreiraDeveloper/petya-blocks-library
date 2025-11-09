b7958f1df029ce43b17f86d5d63f32acad80761f
Petya Naydenova Ferreira <petianaidenova@gmail.com>
2025-11-09 11:09:52 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Petya Block Library
+
+Petya Block Library is a Wordpress plugin that provides a collection of custom blocks for the block editor.
+
+## Blocks
+
---
+++ petya-block-library.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name:       Petya Block Library
+ * Plugin URI:        https://petyaferreira.com
+ * Description:       Petya Block Library is a collection of custom blocks.
+ * Version:           1.0.0
+ * Requires at least: 6.6
+ * Requires PHP:      7.2
+ * Author:            Petya
+ * Author URI:        https://petyaferreira.com
+ * License:           GPL-2.0-or-later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       petya-block-library
+ *
+ * @package           petya-block-library
+ */
