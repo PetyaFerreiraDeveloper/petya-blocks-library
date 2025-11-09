b9b9e897d660e3a7242552d4bdab7951ceb4aa37
Petya Naydenova Ferreira <petianaidenova@gmail.com>
2025-11-09 11:53:20 +0100

    add tab block

---
+++ src/blocks/tab/edit.js
@@ -0,0 +1,9 @@
+/**
+ * Wordpress dependencies
+ */
+
+import { useBlockProps, InnerBlocks } from '@wordpress/block-editor';
+
+export default function Edit() {
+  return <p>Hello from the editor</p>;
+}
